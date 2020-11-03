d610fb04653f109daf61347fb2505a67c05a691c
Amelia Roberts <arob0017@student.monash.edu>
2020-11-04 08:03:01 +1100

    schema added

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS burger_db;
+CREATE DATABASE burger_db;
+USE burger_db;
+
+CREATE TABLE burgers (
+    id INT AUTO_INCREMENT,
+    burger_name VARCHAR(30),
+    devoured BOOLEAN default false,
+    PRIMARY KEY (id) 
+)
