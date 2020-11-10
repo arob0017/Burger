553b3bbcff41da11f2c3bfc494931c21497ca1d5
Amelia Roberts <arob0017@student.monash.edu>
2020-11-11 06:23:47 +1100

    controller setup

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,6 +4,16 @@ const burger = require("../models/burger");
 const router = express.Router();
 
 
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
+        var hbsObject = {
+            burger: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
 router.post("/api/burgers", (req, res) => {
     console.log(req.body)
     burger.insertOne(["burger_name"], [req.body.burger_name], (result) => {
@@ -12,15 +22,29 @@ router.post("/api/burgers", (req, res) => {
     })
 })
 router.put("/api/burgers/:id", (req, res) => {
-    burger.insertOne(["burger_name"], [req.body.burger_name], (result) => {
-        res.json({ msg: "Burger created" })
+    var condition = "id = " + req.params.id;
+    burger.updateOne({ devoured: req.body.devoured }, condition, (result) => {
+        res.json({ msg: "Burger updated" })
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
 
-    })
-})
+    });
+});
+
 router.delete("/api/burgers/:id", (req, res) => {
-    burger.insertOne(["burger_name"], [req.body.burger_name], (result) => {
-        res.json({ msg: "Burger created" })
-
+    var condition = "id = " + req.params.id;
+    burger.delete(condition, (result) => {
+        res.json({ msg: "Burger deleted" })
+        if (result.affectedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
     })
 })
 
