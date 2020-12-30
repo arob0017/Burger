ebfe680720f0369456e020a30f95337b3a451e70
Amelia Roberts <arob0017@student.monash.edu>
2020-12-31 09:51:58 +1100

    jaws url

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mysql = require('mysql');
 const util = require("util");
 
-const connection = mysql.createConnection({
+const connection = mysql.createConnection(process.env.JAWSDB_URL || {
     host: 'localhost',
     port: 3306,
     user: "root",
